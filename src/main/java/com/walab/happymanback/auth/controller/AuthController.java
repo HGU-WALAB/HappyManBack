9c8a9386f3823e2f5518edaddc633b8ccd06b959
LimYeonKyuu <lim000606@naver.com>
2024-03-07 12:33:20 +0900

    docs: swagger 적용 완료

--- src/main/java/com/walab/happymanback/auth/controller/AuthController.java
+++ src/main/java/com/walab/happymanback/auth/controller/AuthController.java
@@ -4,6 +4,16 @@ import com.walab.happymanback.auth.controller.request.TestLoginRequest;
 import com.walab.happymanback.auth.controller.response.TestLoginResponse;
 import com.walab.happymanback.auth.dto.AuthDto;
 import com.walab.happymanback.auth.service.AuthService;
+import com.walab.happymanback.base.response.ExceptionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/happyman/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "로그인 및 회원가입")
 public class AuthController {
 
   private final AuthService authService;
 
   @PostMapping("/test-login")
+  @Operation(summary = "테스트용 로그인", description = "로컬에서 간단하게 돌릴 수 있는 테스트용 로그인 API")
+  @Parameters({
+    @Parameter(name = "uniqueId", description = "학번이나 직번", required = true, example = "21900613"),
+  })
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "로그인 성공"),
+        @ApiResponse(
+            responseCode = "400",
+            description = "로그인 실패",
+            content =
+                @Content(
+                    schema = @Schema(implementation = ExceptionResponse.class),
+                    examples =
+                        @ExampleObject(
+                            value =
+                                "{\n  \"error\": \"BAD_REQUEST\",\n  \"message\": \"로그인 실패\"\n}")))
+      })
   public ResponseEntity<TestLoginResponse> testLogin(@RequestBody TestLoginRequest request) {
-    return ResponseEntity.ok(
-        TestLoginResponse.from(authService.login(AuthDto.from(request))));
+    return ResponseEntity.ok(TestLoginResponse.from(authService.login(AuthDto.from(request))));
   }
 }
